fdc381410086ebd972525a0a1cfe0e2c56eda69d
Völcsey Zoltán <41576384+zvolcsey@users.noreply.github.com>
2024-02-21 23:04:08 +0100

    feat: First commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+function App() {
+  
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        {/* TODO: Game Board */}
+      </div>
+      {/* TODO: Log */}
+    </main>
+  )
+}
+
+export default App
