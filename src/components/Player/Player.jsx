b63a93041741a01347c9aac9a7d83af088e22a4e
Völcsey Zoltán <41576384+zvolcsey@users.noreply.github.com>
2024-02-22 13:30:48 +0100

    feat: Made player name editable

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,12 +1,42 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+  
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>
+  let btnCaption = 'Edit';
+
+  if (isEditing) {
+    editablePlayerName = 
+      <input 
+        className="player-name" 
+        type="text" 
+        required 
+        value={playerName}
+        onChange={handleChange}
+      />;
+    btnCaption = 'Save';
+  }
 
   return (
     <li>
-      <span className="player-info">
-        <span className="player-name">{name}</span>
+      <span className="player">
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>
+        {/* Or ternary operator approach: isEditing ? 'Save' : 'Edit' */}
+        {btnCaption}
+      </button>
     </li>
   )
 }
