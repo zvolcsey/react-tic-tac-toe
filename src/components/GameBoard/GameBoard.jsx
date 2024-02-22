3235e91b42c3d60610effa80946e7b31311e6d70
Völcsey Zoltán <41576384+zvolcsey@users.noreply.github.com>
2024-02-22 23:21:17 +0100

    feat: Added select square functionality

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./GameBoard.css";
 
 const initialGameBoard = [
@@ -7,14 +8,26 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+      updatedGameBoard[rowIndex][colIndex] = 'X';
+      return updatedGameBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) =>
+      {gameBoard.map((row, rowIndex) =>
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button
+                  onClick={() => handleSelectSquare(rowIndex, colIndex)}
+                >{playerSymbol}</button>
               </li>
             ))}
           </ol>
