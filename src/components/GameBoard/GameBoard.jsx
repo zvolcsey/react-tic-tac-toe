ff13ec7fed9954eb9ccd6112ce41655c287b981b
Völcsey Zoltán <41576384+zvolcsey@users.noreply.github.com>
2024-02-23 23:28:50 +0100

    feat: Disabled buttons coditionally

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -25,6 +25,7 @@ export default function GameBoard({ onSelectSquare, turns }) {
               <li key={colIndex}>
                 <button
                   onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
                 >
                   {playerSymbol}
                 </button>
