8c4a64dc9dbc2a758efc9140b18b9e40a459a25b
Völcsey Zoltán <41576384+zvolcsey@users.noreply.github.com>
2024-02-23 23:22:10 +0100

    feat: Reduced state management

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,28 @@ import Player from "./components/Player/Player";
 import GameBoard from "./components/GameBoard/GameBoard";
 import Log from './components/Log/Log';
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    // Update the array of game turns
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
-    
     // Update the GameTurn in immutable way
     setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
-
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+      const activePlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
-        { square: {row: rowIndex, col: colIndex}, player: currentPlayer }
+        { square: {row: rowIndex, col: colIndex}, player: activePlayer }
         , ...prevTurns
       ];
 
